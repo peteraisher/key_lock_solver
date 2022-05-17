d202433ace63a039b532c0d88ad8fc4fa9586ba4
Peter Aisher <peter.aisher@me.com>
2022-05-17 02:20:51 +0200

    tidy Volume

--- key_lock_solver/spatial/Volume.cpp
+++ key_lock_solver/spatial/Volume.cpp
@@ -8,3 +8,29 @@
 
 #include "../../key_lock_solver/spatial/Volume.hpp"
 
+bool Volume::pointsIntersect(const Volume& other, Vec3 offset) const {
+  return std::any_of(points.begin(), points.end(), [&](const auto& p) {
+    return other.points.find(p + offset) != other.points.end();
+  });
+}
+
+Volume::Volume(std::initializer_list<char> il)
+  : boundingBox {std::numeric_limits<Vec3>::max(),
+    std::numeric_limits<Vec3>::min()} {
+  points.reserve(il.size() / 3);
+    if (il.size() % 3) {
+      throw std::runtime_error(
+        "Size of initializer list must be a multiple of three.");
+    }
+  auto p = il.begin();
+  const auto end = il.end();
+  while (p < end) {
+    char x = *(p++);
+    char y = *(p++);
+    char z = *(p++);
+    Vec3 point = {x, y, z};
+    points.insert(point);
+    boundingBox.min = simd_min(boundingBox.min, point);
+    boundingBox.max = simd_max(boundingBox.max, point);
+  }
+}
--- key_lock_solver/spatial/Volume.hpp
+++ key_lock_solver/spatial/Volume.hpp
@@ -21,48 +21,11 @@ typedef std::unordered_set<Vec3> PointSet;
 struct Volume {
   BoundingBox boundingBox {};
   PointSet points {};
-  inline bool pointsIntersect(const Volume& other, Vec3 offset) const {
-    for (const auto& p : points) {
-      if (other.points.find(p + offset) != other.points.end()) {
-        return true;
-      }
-    }
-    return false;
-  }
+  bool pointsIntersect(const Volume& other, Vec3 offset) const;
   inline bool boundingBoxIntersects(const Volume& other, Vec3 offset) const {
     return boundingBox.intersects(other.boundingBox, offset);
   }
-  Volume(std::initializer_list<Vec3> il) : points(il) {
-    constexpr char char_max = std::numeric_limits<char>::max();
-    constexpr char char_min = std::numeric_limits<char>::min();
-    Vec3 bMin {char_max, char_max, char_max};
-    Vec3 bMax {char_min, char_min, char_min};
-    for (const auto& p : points) {
-      bMin = simd_min(bMin, p);
-      bMax = simd_max(bMax, p);
-    }
-    boundingBox = {bMin, bMax};
-  }
-  Volume(std::initializer_list<char> il)
-  : boundingBox {std::numeric_limits<Vec3>::max(),
-    std::numeric_limits<Vec3>::min()}{
-    points.reserve(il.size() / 3);
-      if (il.size() % 3) {
-        throw std::runtime_error(
-          "Size of initializer list must be a multiple of three.");
-      }
-    auto p = il.begin();
-    const auto end = il.end();
-    while (p < end) {
-      char x = *(p++);
-      char y = *(p++);
-      char z = *(p++);
-      Vec3 point = {x, y, z};
-      points.insert(point);
-      boundingBox.min = simd_min(boundingBox.min, point);
-      boundingBox.max = simd_max(boundingBox.max, point);
-    }
-  }
+  Volume(std::initializer_list<char> il);
 };
 
 #endif  // KEY_LOCK_SOLVER_SPATIAL_VOLUME_HPP_
