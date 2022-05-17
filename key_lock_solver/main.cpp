812454c476df90dcc992b07ee3525d1f2499cabf
Peter Aisher <peter.aisher@me.com>
2022-05-17 10:10:00 +0200

    tidy main

--- key_lock_solver/main.cpp
+++ key_lock_solver/main.cpp
@@ -10,24 +10,7 @@
 #include "search/AStarSolver.hpp"
 
 int main(int argc, const char * argv[]) {
-  // insert code here...
-  std::cout << "Hello, World!\n";
-
   auto solver = AStarSolver();
-  KeyLockPuzzleState start {{
-    Vec3{0, 0, 0},  // A
-    {0, 0, 0},      // B
-    {0, 0, 0},      // C
-    {0, 0, 0},      // D
-    {0, 0, 0},      // E
-    {0, 0, 0},      // F
-    {0, 0, 0},      // G
-    {0, 0, 0},      // H
-    {0, 0, 0}       // K
-  }};
-
-//  solver.reset(start);
-//  solver.solve(start);
   solver.solve();
   return 0;
 }
